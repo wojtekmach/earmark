c8b16aea868f6804eba6b98a94f1840c1eb52446
RobertDober <robert.dober@gmail.com>
2017-12-03 11:28:36 +0100

    lib/earmark/helpers/leex_helpers.ex correctly speced

--- lib/earmark/helpers/leex_helpers.ex
+++ lib/earmark/helpers/leex_helpers.ex
@@ -3,6 +3,7 @@ defmodule Earmark.Helpers.LeexHelpers do
     Allows to lex an Elixir string with a leex lexer and returns
     the tokens as needed for a yecc parser.
   """
+  @spec lex( String.t, Keyword.t(atom()) ) :: list(lex_token_type)
   def lex text, with: lexer do
     case text
       |> String.to_charlist()
@@ -11,6 +12,7 @@ defmodule Earmark.Helpers.LeexHelpers do
       end
   end
 
+  @spec tokenize( String.t, Keyword.t(atom()) ) :: list(elx_token_type) 
   def tokenize line, with: lexer do
     {:ok, tokens, _} =
     line
@@ -20,6 +22,10 @@ defmodule Earmark.Helpers.LeexHelpers do
     |> Enum.reverse()
   end
 
+  @typep lex_token_type :: { atom(), number(), charlist() }
+  @typep elx_token_type :: { atom(), String.t }
+
+  @spec elixirize_tokens( list(lex_token_type), list(elx_token_type) ) :: list(elx_token_type)
   defp elixirize_tokens(tokens, rest)
   defp elixirize_tokens([], result), do: result
   defp elixirize_tokens([{token, _, text}|rest], result), do: elixirize_tokens(rest, [{token,to_string(text)}|result])
