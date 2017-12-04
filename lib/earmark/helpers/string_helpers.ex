6b5f7000e3029252fbbe9b9e96bef1e6dd20e745
RobertDober <robert.dober@gmail.com>
2017-12-04 21:00:15 +0100

    lib/earmark/helpers/string_helpers.ex correctly speced

--- lib/earmark/helpers/string_helpers.ex
+++ lib/earmark/helpers/string_helpers.ex
@@ -3,6 +3,7 @@ defmodule Earmark.Helpers.StringHelpers do
   @doc """
   Remove the leading part of a string
   """
+  @spec behead( String.t, String.t | number() ) :: String.t
   def behead(str, ignore) when is_integer(ignore) do
     String.slice(str, ignore..-1)
   end
@@ -17,7 +18,7 @@ defmodule Earmark.Helpers.StringHelpers do
         iex> behead_tuple("prefixpostfix", "prefix")
         {"prefix", "postfix"}
   """
-#   @spec behead_tuple( String.t, String.t ) :: {String.t, String.t}
+  @spec behead_tuple( String.t, String.t ) :: {String.t, String.t}
   def behead_tuple(str, lead) do
     {lead, behead(str, lead)}
   end
