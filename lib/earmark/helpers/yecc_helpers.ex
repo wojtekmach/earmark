a706156ee34e870eaa103a81c912f760acad4eba
RobertDober <robert.dober@gmail.com>
2017-12-04 21:10:09 +0100

    lib/earmark/helpers/yecc_helpers.ex correctly speced

--- lib/earmark/helpers/yecc_helpers.ex
+++ lib/earmark/helpers/yecc_helpers.ex
@@ -1,6 +1,10 @@
 defmodule Earmark.Helpers.YeccHelpers do
+  use Earmark.Types
+
   import Earmark.Helpers.LeexHelpers, only: [lex: 2]
 
+  @doc false
+  @spec parse!( String.t, Keyword.t(atom()) ) :: maybe({String.t, String.t})
   def parse!( text, lexer: lexer, parser: parser ) do
     case parse(text, lexer: lexer, parser: parser) do
         {:ok, ast}  -> ast |> elixirize()
@@ -8,11 +12,14 @@ defmodule Earmark.Helpers.YeccHelpers do
     end
   end
 
+  @doc false
+  @spec parse( String.t, Keyword.t(atom()) ) ::  {:ok, {charlist(), charlist()}} | {:error, any()}
   def parse( text, lexer: lexer, parser: parser ) do
     with tokens <- lex(text, with: lexer) do
       parser.parse(tokens)
     end
   end
 
+  @spec elixirize( {charlist(), charlist()} ) :: {String.t, String.t} 
   defp elixirize({l, r}), do: { to_string(l), to_string(r) }
 end
